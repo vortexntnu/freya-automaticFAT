9f04afabbda441c163f6ea211eba17dbb7ffd7f8
Fabian Kongelf <fkongelf@gmail.com>
2024-01-11 11:45:48 +0100

    update

--- src/tools/cli_tools.py
+++ src/tools/cli_tools.py
@@ -1,33 +1,27 @@
 #!/usr/bin/env python
 
 import subprocess
+from typing import Union
 
 # run command, output true/false => command success or not
-def run_bool(command: str, device: object = None) -> bool:
+def run_bool(command: str) -> bool:
     try:
-        if device:
-            command = ssh_wrap(command, device)
+        result = subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
-        result = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-
-        # Check the command's return code (0 means success)
-        return result == 0
+        return result.returncode == 0
     
     except Exception as e:
         return False
 
 # run command, output text
-def run_str(command: str, device: object = None) -> str:
+def run_str(command: str) -> (bool, str):
     try:
-        if device:
-            command = ssh_wrap(command, device)
-
         result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-
+        
         return result.decode("utf-8")
 
     except Exception as e:
-        return False
+        return ""
 
 
 # add ssh wrapping to command
